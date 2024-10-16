ead125d443aa5cfbca57bf4bf50f086622404667
paulo-rago <149490954+paulo-rago@users.noreply.github.com>
2024-10-16 12:19:26 -0300

    commit transacao

--- titulos/src/br/com/cesarschool/poo/titulos/entidades/Transacao.java
+++ titulos/src/br/com/cesarschool/poo/titulos/entidades/Transacao.java
@@ -7,13 +7,75 @@ package br.com.cesarschool.poo.titulos.entidades;
  * tituloDivida, do tipo TituloDivida
  * valorOperacao, do tipo double
  * dataHoraOperacao, do tipo LocalDateTime
- *  
- * Deve ter um construtor público que inicializa os atributos.
- * Deve ter métodos get/set públicos para todos os atributos, que 
- * são read-only fora da classe.
+ *
+ * Deve ter um construtor pï¿½blico que inicializa os atributos.
+ * Deve ter mï¿½todos get/set pï¿½blicos para todos os atributos, que 
+ * sï¿½o read-only fora da classe.
  * 
  *  
  */ 
 public class Transacao {
+    private EntidadeOperadora entidadeCredito;
+    private EntidadeOperadora entidadeDebito;
+    private Acao acao;
+    private TituloDivida tituloDivida;
+    private double valorOperacao;
+    private LocalDateTime dataHoraOperacao;
 
+    public Transacao(EntidadeOperadora entidadeCredito, EntidadeOperadora entidadeDebito, Acao acao, TituloDivida tituloDivida, double valorOperacao, LocalDateTime dataHoraOperacao){
+        this.entidadeCredito = entidadeCredito;
+        this.entidadeDebito = entidadeDebito;
+        this.acao = acao;
+        this.tituloDivida = tituloDivida;
+        this.valorOperacao = valorOperacao;
+        this.dataHoraOperacao = dataHoraOperacao;
+    }
+
+    public EntidadeOperadora getEntidadeCredito() {
+        return entidadeCredito;
+    }
+
+    public EntidadeOperadora getEntidadeDebito() {
+        return entidadeDebito;
+    }
+
+    public Acao getAcao() {
+        return acao;
+    }
+
+    public TituloDivida getTituloDivida() {
+        return tituloDivida;
+    }
+
+    public double getValorOperacao() {
+        return valorOperacao;
+    }
+
+    public LocalDateTime getDataHoraOperacao() {
+        return dataHoraOperacao;
+    }
+
+    public void setEntidadeCredito(EntidadeOperadora entidadeCredito) {
+        this.entidadeCredito = entidadeCredito;
+    }
+
+    public void setEntidadeDebito(EntidadeOperadora entidadeDebito) {
+        this.entidadeDebito = entidadeDebito;
+    }
+
+    public void setTituloDivida(TituloDivida tituloDivida) {
+        this.tituloDivida = tituloDivida;
+    }
+
+    public void setAcao(Acao acao) {
+        this.acao = acao;
+    }
+
+    public void setValorOperacao(double valorOperacao) {
+        this.valorOperacao = valorOperacao;
+    }
+
+    public void setDataHoraOperacao(LocalDateTime dataHoraOperacao) {
+        this.dataHoraOperacao = dataHoraOperacao;
+    }
 }
