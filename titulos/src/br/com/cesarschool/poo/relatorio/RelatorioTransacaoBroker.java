c381b0009f16f859b4f024795ee0b509e179a337
luana <llmaf@cesar.school>
2024-11-23 15:58:30 -0300

    packges

--- titulos/src/br/com/cesarschool/poo/relatorio/RelatorioTransacaoBroker.java
+++
@@ -1,5 +0,0 @@
-package br.com.cesarschool.poo.relatorio;
-
-public class RelatorioTransacaoBroker {
-
-}
---
+++ titulos/src/br/com/cesarschool/poo/titulos/relatorios/RelatorioTransacaoBroker/RelatorioTransacaoBroker.java
@@ -0,0 +1,5 @@
+package br.com.cesarschool.poo.titulos.relatorios.RelatorioTransacaoBroker;
+
+public class RelatorioTransacaoBroker {
+
+}
