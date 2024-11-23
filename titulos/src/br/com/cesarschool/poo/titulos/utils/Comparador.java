c7e06a5d0ed7458a5b965f85df226742118a7e0c
luana <llmaf@cesar.school>
2024-11-23 16:51:04 -0300

    push

--- titulos/src/br/com/cesarschool/poo/titulos/utils/Comparador.java
+++ titulos/src/br/com/cesarschool/poo/titulos/utils/Comparador.java
@@ -1,7 +1,5 @@
 package br.com.cesarschool.poo.titulos.utils;
 
-import br.com.cesarschool.poo.titulos.utils.Comparavel;
-
 public interface Comparador {
 
     abstract int comparar(Comparavel c1, Comparavel c2);
--- titulos/src/br/com/cesarschool/poo/titulos/utils/Ordenador.java
+++ titulos/src/br/com/cesarschool/poo/titulos/utils/Ordenador.java
@@ -1,10 +1,27 @@
 package br.com.cesarschool.poo.titulos.utils;
 
 public class Ordenador {
-	// ordena o array de entrada perguntando ao
-    // comparador se dois elementos são maiores, menores ou iguais.
 	public static void ordenar(Comparavel[] ents, Comparador comp) {
-		
+        for (int i = 0; i < ents.length - 1; i++) {
+            for (int j = 0; j < ents.length - 1 - i; j++) {
+                if (comp.comparar(ents[j], ents[j + 1]) > 0) {
+                	Comparavel temp = ents[j];
+                    ents[j] = ents[j + 1];
+                    ents[j + 1] = temp;
+                }
+            }
+        }
+    }
+	
+	public static void ordenar(Comparavel[] comps) {
+		for (int i = 0; i < comps.length - 1; i++) {
+	        for (int j = 0; j < comps.length - 1 - i; j++) {
+	            if (comps[j].comparar(comps[j + 1]) > 0) {
+	                Comparavel temp = comps[j];
+	                comps[j] = comps[j + 1];
+	                comps[j + 1] = temp;
+	            }
+	        }
+	    }
 	}
-
 }
