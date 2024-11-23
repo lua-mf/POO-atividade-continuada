7b47aaf5ba29dafb3e8ac0b60c77d081bba0ea0f
luana <llmaf@cesar.school>
2024-11-23 09:39:46 -0300

    classe Entidade

--- titulos/src/br/gov/cesarschool/poo/daogenerico/Entidade.java
+++ titulos/src/br/gov/cesarschool/poo/daogenerico/Entidade.java
@@ -1,4 +1,8 @@
 package br.gov.cesarschool.poo.daogenerico;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
 /*
  * Esta classe representa uma superclasse de todas as entidades. 
  * 
@@ -17,6 +21,51 @@ package br.gov.cesarschool.poo.daogenerico;
  * Deve implementar a interface Serializable do JAVA
  */
 
-public abstract class Entidade {
+public abstract class Entidade implements Serializable {
+    private static final long serialVersionUID = 1L; // Controle de versão da serialização
 
+	private LocalDateTime dataHoraInclusao;
+	private LocalDateTime dataHoraUltimaAlteracao;
+	private String usuarioInclusao;
+	private String usuarioUltimaAlteracao;
+	
+
+	public Entidade() {
+		
+	}
+
+	public LocalDateTime getDataHoraInclusao() {
+		return dataHoraInclusao;
+	}
+
+	public void setDataHoraInclusao(LocalDateTime dataHoraInclusao) {
+		this.dataHoraInclusao = dataHoraInclusao;
+	}
+
+	public LocalDateTime getDataHoraUltimaAlteracao() {
+		return dataHoraUltimaAlteracao;
+	}
+
+	public void setDataHoraUltimaAlteracao(LocalDateTime dataHoraUltimaAlteracao) {
+		this.dataHoraUltimaAlteracao = dataHoraUltimaAlteracao;
+	}
+
+	public String getUsuarioInclusao() {
+		return usuarioInclusao;
+	}
+
+	public void setUsuarioInclusao(String usuarioInclusao) {
+		this.usuarioInclusao = usuarioInclusao;
+	}
+
+	public String getUsuarioUltimaAlteracao() {
+		return usuarioUltimaAlteracao;
+	}
+
+	public void setUsuarioUltimaAlteracao(String usuarioUltimaAlteracao) {
+		this.usuarioUltimaAlteracao = usuarioUltimaAlteracao;
+	}
+	
+	public abstract int getIdUnico();
+	
 }
