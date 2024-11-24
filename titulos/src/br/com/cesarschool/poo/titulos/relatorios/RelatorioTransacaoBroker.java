828dd67f7ff3eebbb4ed3291b83e1189436ce092
luana <llmaf@cesar.school>
2024-11-24 18:02:07 -0300

    metodo de relatorioTransacaoPorNomeEntidadeCredora

--- titulos/src/br/com/cesarschool/poo/titulos/relatorios/RelatorioTransacaoBroker.java
+++ titulos/src/br/com/cesarschool/poo/titulos/relatorios/RelatorioTransacaoBroker.java
@@ -1,5 +1,30 @@
 package br.com.cesarschool.poo.titulos.relatorios;
 
+import br.com.cesarschool.poo.titulos.utils.Ordenador;
+import br.com.cesarschool.poo.titulos.entidades.Transacao;
+import br.com.cesarschool.poo.titulos.repositorios.RepositorioTransacao;
+import br.com.cesarschool.poo.titulos.utils.Comparador;
+import br.com.cesarschool.poo.titulos.utils.ComparadorTransacaoPorNomeCredora;
+import br.com.cesarschool.poo.titulos.utils.Comparavel;
+
 public class RelatorioTransacaoBroker {
 
+    private RepositorioTransacao repositorio;
+
+    public RelatorioTransacaoBroker() {
+        this.repositorio = new RepositorioTransacao();
+    }
+
+    public Transacao[] relatorioTransacaoPorNomeEntidadeCredora() {
+        // Busca todas as transações
+        Transacao[] transacoes = repositorio.buscarPorEntidadeCredora(-1); 
+        if (transacoes == null || transacoes.length == 0) {
+            return new Transacao[0]; 
+        }
+
+        // Ordena usando o ComparadorTransacaoPorNomeCredora
+        Ordenador.ordenar(transacoes, new ComparadorTransacaoPorNomeCredora());
+
+        return transacoes;
+    }
 }
--- titulos/src/br/com/cesarschool/poo/titulos/utils/ComparadorTransacaoPorNomeCredora.java
+++ titulos/src/br/com/cesarschool/poo/titulos/utils/ComparadorTransacaoPorNomeCredora.java
@@ -1,5 +1,17 @@
 package br.com.cesarschool.poo.titulos.utils;
 
-public class ComparadorTransacaoPorNomeCredora {
+import br.com.cesarschool.poo.titulos.entidades.Transacao;
+
+public class ComparadorTransacaoPorNomeCredora extends ComparadorPadrao implements Comparador {
+	public int comparar(Comparavel c1, Comparavel c2) {
+        if (!(c1 instanceof Transacao) || !(c2 instanceof Transacao)) {
+            throw new IllegalArgumentException("Os objetos devem ser do tipo Transacao.");
+        }
+
+        Transacao t1 = (Transacao) c1;
+        Transacao t2 = (Transacao) c2;
+
+        return t1.getEntidadeCredito().getNome().compareTo(t2.getEntidadeCredito().getNome());
+    }
 
 }
